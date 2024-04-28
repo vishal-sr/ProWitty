36a4332b466d90b61730cc3ccff3dcf8f915daea
vishal.sr@hotmail.com <vishal.sr@hotmail.com>
2024-04-28 10:15:37 +0530

    Add transcript store feature

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from aws_service import get_transcipt
+from aws_service import get_transcript
 def store():
     pass
 
@@ -9,6 +9,10 @@ def S3_loader():
     pass
 
 def write_transcript_file(fileDirectory: str):
-    transcript = get_transcipt(fileDirectory)
-    with open("storage/files/transcript.txt") as file:
+    transcript = get_transcript(fileDirectory)
+    with open("storage/files/transcript.txt", "a") as file:
         file.write(transcript)
+
+
+if __name__ == "__main__":
+    write_transcript_file(r"C:\Users\vishal\Documents\AI\ProWitty\storage\files\videos\awsAI15316677.autosave.mp4")
